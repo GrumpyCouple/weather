ef3d31e88361629c149e9142b2d548e1c26c4742
efghijkaytee <efghijkaytee@gmail.com>
2016-01-02 17:03:30 -0500

    update weather.js

--- dev/assets/js/weather.js
+++ dev/assets/js/weather.js
@@ -21,3 +21,41 @@ function switchUnits(){
 	weatherData.temperatureValue + weatherData.units + ", ";
 }
 
+function getLocationAndWeather() {
+	if (window.XMLHttpRequest) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.addEventListener("load", function() {var response = JSON.parse(xhr.responseText);
+
+			console.log(response);
+			var position = {
+				latitude: response.latitude
+				longitude: response.longitude
+			};
+			var cityName = response.city;
+
+			var weatherSimpleDescription =
+		response.weather.simple;
+			var weatherDescription =
+		response.weather.description;
+			var weatherTemperature =
+		roundTemperature(response.weather.temperature);
+
+			weatherData.temperatureValue = weatherTemperature;
+
+			weatherData.city.innerHTML = cityName;
+			weatherData.weather.innerHTML = ", " + weatherDescription;
+			weatherdata.temperature.innerHTML = weatherTemperature + weatherData.units;
+		}, false);
+		xhr.addEventListener("error", function(err){alert("Could not complete the request");
+	}, false);
+
+		xhr.open("GET", "https://api.forecast.io/forecast/APIKEY/LATITUDE,LONGITUDE", true);
+		xhr.send();
+
+	}
+	else{
+		alert("Unable to fetch the location and weather data.");
+		}
+	}
+}
